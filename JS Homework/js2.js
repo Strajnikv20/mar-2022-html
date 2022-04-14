f6b55ba0f81385f663c5586286464b2d7b2e8005
Strajnikv20 <Lemeshchuk2017@gmail.com>
2022-04-14 14:41:31 +0300

    Homework js2 1

---
+++ JS Homework/js2.js
@@ -0,0 +1,41 @@
+let height = 23
+let width = 10
+let s =height*width
+console.log(s)
+
+let heightC = 10
+let dC = 4
+let v = heightC*Math.PI*4*4
+console.log(v)
+
+let n =3
+let m =4
+let k =n**2+m**2
+console.log(Math.sqrt(k))
+
+let a=5<6
+console.log(a)
+let b=5>6
+console.log(b)
+let c=5>=6
+console.log(c)
+let aa=5==6
+console.log(aa)
+let bb=10<=10
+console.log(bb)
+let cc=10>=10
+console.log(cc)
+let aaa=10!=10
+console.log(aaa)
+let bbb=10!=10
+console.log(bbb)
+let ccc='123'>123
+console.log(ccc)
+let naz='123'>=123
+console.log(naz)
+
+
+
+
+
+
