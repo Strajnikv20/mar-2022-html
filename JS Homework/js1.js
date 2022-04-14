edfb5314f6158488120946365e0686cee8102772
Strajnikv20 <Lemeshchuk2017@gmail.com>
2022-04-14 14:06:39 +0300

    Homework js 1

---
+++ JS Homework/js1.js
@@ -0,0 +1,86 @@
+// let hello = 'hello'
+// let owu = 'owu'
+// let com = 'com'
+// let a = 1
+// let b = 10
+// let c = -999
+// let aa = 123
+// let bb = 3.14
+// let cc = 2.7
+// let aaa = 16
+// let bbb = true
+// let ccc = false
+// console.log(hello)
+// console.log(owu)
+// console.log(com)
+// console.log(a)
+// console.log(b)
+// console.log(c)
+// console.log(aa)
+// console.log(bb)
+// console.log(cc)
+// console.log(aaa)
+// console.log(bbb)
+// console.log(ccc)
+// alert(hello)
+// alert(owu)
+// alert(com)
+// alert(a)
+// alert(b)
+// alert(c)
+// alert(aa)
+// alert(bb)
+// alert(cc)
+// alert(aaa)
+// alert(bbb)
+// alert(ccc)
+// document.write(hello+" ")
+// document.write(owu+" ")
+// document.write(com+" ")
+// document.write(a+" ")
+// document.write(b+" ")
+// document.write(c+" ")
+// document.write(aa+" ")
+// document.write(bb+" ")
+// document.write(cc+" ")
+// document.write(aaa+" ")
+// document.write(bbb+" ")
+// document.write(ccc+" ")
+// let book = {
+//     name:"kniga",
+//     pageCount:100,
+//     janr:"drama",
+//     authors:["user","user1","user2"]
+// }
+// let firstName = 'Volodymyr'
+// let mіddleName = 'Ruslanovich'
+// let lastName = 'Lemeshchuk'
+// let person = firstName + " " + mіddleName + " " + lastName
+// console.log(person)
+// let firstName = prompt('enter first name', 'Vova')
+// console.log(firstName)
+// let middleName = prompt('enter your middle name', 'Ruslanovich')
+// console.log(middleName)
+// let age = prompt('enter your age', '21')
+// console.log(age)
+// let a = 100
+// let b = '100'
+// let c = true
+// console.log(typeof a)
+// console.log(typeof b)
+// console.log(typeof c)
+// let books = [
+//     {
+//         name: "kniga1",
+//         pageCount: 100,
+//         janr: "horor",
+//         authors: ["user3", "user4", "user5"]
+//     }, {
+//         name: "kniga2",
+//         pageCount: 200,
+//         janr: "roman",
+//         authors: ["user6", "user7", "user8"]
+//     }
+// ]
+// console.log(books[0])
+// console.log(books[1])
