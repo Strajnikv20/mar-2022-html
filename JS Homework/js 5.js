81b8ebb76a4c3f88631f9f60a636031bff31c194
Strajnikv20 <Lemeshchuk2017@gmail.com>
2022-04-26 14:22:50 +0300

    Homework 4 js5

---
+++ JS Homework/js 5.js
@@ -0,0 +1,98 @@
+// function plocha(a, b) {
+//     let result = (a * b);
+//     return result;
+// }
+//
+// let answer = plocha(10, 20);
+// document.write(answer);
+//
+//
+// function kolo(r) {
+//     let result = 3.14 * (r) * 2;
+//     return result;
+// }
+//
+// let qwe = kolo(20);
+// document.write(qwe)
+
+// function radius(h, r) {
+//     let result = (2 * 3.14 * r) + (2 * 3.14 * r * h)
+//     return result;
+// }
+//
+// let asd = radius(15, 20);
+// document.write(asd)
+
+// function a(array) {
+//     for (let item of array) {
+//         document.write(item);
+//
+//     }
+// }
+//
+// let skills = [`java`, ` `, `js`, ` `, `react`, ` `, `html`];
+// a(skills)
+
+// function text(a) {
+//     document.write(a)
+// }
+//
+// text(`<h1>Some text</h1>`);
+//
+
+// function lijka(a) {
+//     document.write(`<ul>`)
+//     document.write(`<li>${a}</li>`)
+//     document.write(`<li>${a}</li>`)
+//     document.write(`<li>${a}</li>`)
+//     document.write(`</ul>`)
+// }
+//
+// lijka(`Some text`);
+
+// function naz(a, num) {
+//     document.write(`<ul>`)
+//     for (let i = 0; i < num; i++) {
+//         document.write(`<li>${a}</li>`)
+//     }
+//     document.write(`</ul>`)
+// }
+//
+// naz(`some text`, 10);
+
+
+// function qwe(array) {
+//     document.write(`<ul>`)
+//
+//     document.write(`<li>${array[0]}</li>`)
+//     document.write(`<li>${array[1]}</li>`)
+//     document.write(`<li>${array[2]}</li>`)
+//     document.write(`<li>${array[3]}</li>`)
+//
+//     document.write(`</ul>`)
+// }
+//
+// let array = [47, `text`, true, false];
+//
+// qwe(array);
+
+
+// function mass(array) {
+//     document.write(`<div>${array[0].id}</div>`)
+//     document.write(`<div>${array[0].name}</div>`)
+//     document.write(`<div>${array[0].age}</div>`)
+//     document.write(`<div>${array[1].id}</div>`)
+//     document.write(`<div>${array[1].name}</div>`)
+//     document.write(`<div>${array[1].age}</div>`)
+//     document.write(`<div>${array[2].id}</div>`)
+//     document.write(`<div>${array[2].name}</div>`)
+//     document.write(`<div>${array[2].age}</div>`)
+// }
+//
+// let array = [
+//     {id: `qwe`, name: `naz`, age: 23},
+//     {id: `asd`, name: `paul`, age: 22},
+//     {id: `zxc`, name: `vova`, age: 22},
+// ];
+//
+// mass(array);
