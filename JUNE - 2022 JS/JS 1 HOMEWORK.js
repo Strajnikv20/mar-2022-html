923686fa7feff7a06b6b641d75c1846d04446a54
Strajnikv20 <Lemeshchuk2017@gmail.com>
2022-07-25 15:37:37 +0300

    JS 1 HOMEWORK NEW

---
+++ JUNE - 2022 JS/JS 1 HOMEWORK.js
@@ -0,0 +1,56 @@
+// - Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+//     Вивести кожну змінну за допомогою: console.log , alert, document.write
+//
+// - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)
+//
+// - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+//     let a = 100; let b = '100'; let c = true;
+//
+// - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками. та вивести в консоль
+
+// let a = `hello`
+// let a1 = `owu`
+// let a2 = `com`
+// let a3 = `ua`
+// let a4 = 1
+// let a5 = 10
+// let a6 = -999
+// let a7 = 123
+// let a8 = 3.14
+// let a9 = 2.7
+// let a10 = 16
+// let a11 = true
+// let a12 = false
+// console.log(a,a1,a2,a3,a4,a5,a6,a7,a8,a9,a10,a11,a12);
+// alert(a);
+// alert(a1);
+// alert(a2);
+// alert(a3);
+// alert(a4);
+// alert(a5);
+// alert(a6);
+// alert(a7);
+// alert(a8);
+// alert(a9);
+// alert(a10);
+// alert(a11);
+// alert(a12);
+// document.write(a,` `,a1,` `,a2,` `,a3,` `,a4,` `,a5,` `,a6,` `,a7,` `,a8,` `,a9,` `,a10,` `,a11,` `,a12)
+// let firstName = `Volodymyr`;
+// let middleName = `Ruslanovich`;
+// let lastName = `Lemeshchuk`;
+// const person = `${firstName} ${middleName} ${lastName}`
+// console.log(person);
+// let a = 100;
+// let b = '100';
+// let c = true;
+// console.log(typeof (a));
+// console.log(typeof (b));
+// console.log(typeof (c));
+// let a = prompt(`Your firstname?`,`Volodymyr`)
+// console.log(a);
+// let b = prompt(`your middlename?`,`Ruslanovich`)
+// console.log(b);
+// let c = prompt(`your last name?`,`Lemeshchuk`)
+// console.log(c);
+
