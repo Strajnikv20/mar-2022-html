b80b9e6b497f63c335892129c5330ca7a3eb2736
Strajnikv20 <Lemeshchuk2017@gmail.com>
2022-07-27 18:04:37 +0300

    JS 1 HOMEWORK NEW

---
+++ JUNE - 2022 JS/JS 2 HOWEWORK.js
@@ -0,0 +1,126 @@
+// - Створити масив, наповнити його 10 елементами будь-якого типу, вивести кожен елемент в консоль
+// - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre.
+// - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre, authors. Поле "автори" - масив. Кожен автор має поля name,age
+// - Створити масив з 10 об'єктами які описують сутніть "користувач". Поля: name, username,password. Вивести в консоль пароль кожного користувача
+
+// let array = [
+//     `vasya`,
+//     `pupkin`,
+//     `31 years`,
+//     666,
+//     true,
+//     false,
+//     `kamaz`,
+//     `wyhopen`,
+//     777,
+//     `olesya`
+// ]
+// console.log(array[0], array[1], array[2], array[3], array[4])
+// console.log(array[5], array[6], array[7], array[8], array[9])
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+// let book1 = {
+//     title: `LORD OF THE RINGS`,
+//     pageCount: 333,
+//     genre: `fantasy`
+// }
+// let book2 = {
+//     title: `GAME OF THRONES`,
+//     pageCount: 444,
+//     genre: `drama`
+// }
+// let book3 = {
+//     title: `666`,
+//     pageCount: 666,
+//     genre: `horor`
+// }
+//
+//
+//
+//
+//
+//
+//
+//
+//
+// let book4 = {
+//     title: `LORD OF THE RINGS`,
+//     pageCount: 333,
+//     genre: `fantasy`,
+//     authors: [
+//         {
+//             name: `Jeff Murphy`,
+//             age: 45
+//         },
+//         {
+//             name: `Peter Jackson`,
+//             age: 62
+//         }
+//     ]
+// }
+//
+// let book5 = {
+//     title: `GAME OF THRONES`,
+//     pageCount: 444,
+//     genre: `drama`,
+//     authors: [
+//         {
+//             name: `George R.R.Martin`,
+//             age: 57
+//         },
+//         {
+//             name: `David Benioff`,
+//             age: 47
+//         }
+//     ]
+
+// }
+// let book6 = {
+//     title: `The deep house`,
+//     pageCount: 666,
+//     genre: `horor`,
+//     authors: [
+//         {
+//             name: `Alexander Bustio`,
+//             age: 42
+//         },
+//         {
+//             name: `Julien Mori`,
+//             age: 38
+//         }
+//     ]
+// }
+
+
+//
+// const users = [
+//     {name:`user1`,username:`Max`,password:`qweqwe123`},
+//     {name:`user2`,username:`Oleg`,password:`qweqwe456`},
+//     {name:`user3`,username:`Anna`,password:`qweqwe789`},
+//     {name:`user4`,username:`Sofie`,password:`qweqwe234`},
+//     {name:`user5`,username:`Maria`,password:`qweqwe456`},
+//     {name:`user6`,username:`Nazar`,password:`qweqwe567`},
+//     {name:`user7`,username:`Volodya`,password:`qweqwe678`},
+//     {name:`user8`,username:`Galia`,password:`qweqwe654`},
+//     {name:`user9`,username:`Ostap`,password:`qweqwe878`},
+//     {name:`user10`,username:`Misha`,password:`qweqwe345`},
+// ]
+// console.log(users[0].password)
+// console.log(users[1].password)
+// console.log(users[2].password)
+// console.log(users[3].password)
+// console.log(users[4].password)
+// console.log(users[5].password)
+// console.log(users[6].password)
+// console.log(users[7].password)
+// console.log(users[8].password)
+// console.log(users[9].password)
+//
